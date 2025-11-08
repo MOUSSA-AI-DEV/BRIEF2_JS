a4db533df4dc18a6d2e375ccf47986bf0fec6886
MOUSSA-AI-DEV <essalhimoussa@gmail.com>
2025-11-08 20:07:23 +0100

    feat(script.js): ajout desobjet de éducation, expérience et objectif professionnel par click sur next et leur recupuration

--- assets/js/script.js
+++ assets/js/script.js
@@ -95,4 +95,83 @@ btn_previece.addEventListener("click", (e) => {
 
 }
 )
+// validation 
+let general_data_object={}
+let personnel_information={}
+let parcours_educatif={}
+let experience_objectife_pro={}
+stepper_Data__Personnel.addEventListener("input" ,(e)=>{
 
+    
+console.log(counter)
+        console.log(stepper_Data__Personnel.querySelectorAll("input"))
+        let inputs_value = stepper_Data__Personnel.querySelectorAll("input , textarea,select")
+        inputs_value.forEach((el)=>{
+            if(el.value){
+// console.log(el.name)
+                personnel_information[el.name] =el.value
+
+            }
+        })
+    
+        console.log(personnel_information)
+
+    })
+stepper_Data__competence.addEventListener("input", (e)=>{
+    
+        console.log(stepper_Data__competence.querySelectorAll("input"))
+        let inpute_competence = stepper_Data__competence.querySelectorAll("input , textarea, select")
+        inpute_competence.forEach((el) => {
+            // let el=e.target
+            if (el.value) {
+
+                parcours_educatif[el.name] = el.value
+
+            }
+            else{
+                return
+            }
+
+        })
+    console.log(parcours_educatif)
+
+    }
+)
+
+stepper_Data__experience.addEventListener("input", (e) => {
+    console.log(stepper_Data__experience.querySelectorAll("input, textarea ,select"))
+    let input_experience = stepper_Data__experience.querySelectorAll("input, textarea ,select")
+    // let el = e.target
+
+ 
+        // console.log(element.name)
+
+    input_experience.forEach(element => {
+            if (element.value) {
+            experience_objectife_pro[element.name] = element.value
+            } else {
+                return
+            }
+        });
+
+    
+
+    console.log(experience_objectife_pro)
+     general_data_object = {
+        ...personnel_information,
+        ...parcours_educatif,
+        ...experience_objectife_pro
+    };
+console.log(general_data_object)
+})  
+ 
+  
+      
+
+
+
+// btn_next.addEventListener("click", (e) => { console.log("hello"+counter) })
+
+const inputs = document.getElementsByClassName("input"); // NodeList
+// inputs.forEach(input => console.log(input));
+// console.log(inputs)
