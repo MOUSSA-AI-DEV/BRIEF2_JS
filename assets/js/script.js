21c4951a0848c9dd3545b0c67e27ca6b88c9cc16
MOUSSA-AI-DEV <essalhimoussa@gmail.com>
2025-11-13 23:58:34 +0100

    feat(script.js): ajout d objet pere pour touts les inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,14 @@
 
 const experience_objectife_pro = {
-    //Informations personnelles
-    informations: {
-       
-        langues: [
-            {
-                
-            }
-        ],
- //plusieurs langues possibles
-    },
-
-    //Image personnelle
+    informations: {},
     photo: "",
-
-    //Parcours Éducatif
-    formation: [
-        {
-           
-            depuis: {
-               
-            },
-            jusqua: {
-               
-            }
-        }
-    ],
-
-    //Compétences multiple
-    competences: [
-        {
-           
-        }
-    ],
-
-    
-   
-    //oisirs multiple
+    formation: [{ depuis: {}, jusqua: {} }],
+    parcours_educatif: {},
+    competences: [],
+    langues: [],
     loisirs: [],
-
-    //supplémentaire
-    extraSection: {
-        titre: "",
-        description: ""
-    },
-
-    //  8 — Expériences
-    experiences: {
-        projets: [
-            {
-            }
-        ],
-        certifications: [
-            {
-               
-            }
-        ],
-        references: [
-            {
-                poste_ou_relation: ""
-            }
-        ]
-    }
+    extraSection: { titre: "", description: "" },
+    experiences: { projets: [], certifications: [], references: {} }
 };
 
 let stepper_Data__competence = document.getElementById("stepper_Data__competence")
@@ -69,10 +16,12 @@ let stepper_Data__experience = document.getElementById("stepper_Data__experience
 let stepper_Data__Personnel = document.getElementById("stepper_Data__Personnel")
 let btn_previece = document.getElementById("previeuce")
 let btn_next = document.getElementById("next")
+let enregistrement_btn = document.getElementById("Enregistrer")
+let template__cv = document.getAnimations("template__cv")
 
-let counter=0
-let array_position = [stepper_Data__Personnel,stepper_Data__competence, stepper_Data__experience]
-stepper_Data__Personnel.style.display ="block"//block
+let counter = 0
+let array_position = [stepper_Data__Personnel, stepper_Data__competence, stepper_Data__experience]
+stepper_Data__Personnel.style.display = "block"//block
 stepper_Data__experience.style.display = "none"
 stepper_Data__competence.style.display = "none"
 console.log(array_position[0])
@@ -81,53 +30,53 @@ console.log(array_position[0])
 // document.getElementById(`cercel__2`).style.backgroundColor = "blue"; //cercel 2
 
 
-btn_next.addEventListener("click" ,(e)=>{
+btn_next.addEventListener("click", (e) => {
 
-if(counter<=1 )
-{
-    counter+=1
-    console.log(counter)}
-   for(let i=0 ;i>=0 && i<=2; i++){
+    if (counter <= 1) {
+        counter += 1
+        console.log(counter)
+    }
+    for (let i = 0; i >= 0 && i <= 2; i++) {
         console.log(array_position[i])
-       if (counter == 2) {
-           document.getElementById(`line__2`).style.backgroundColor = "blue";//line 2
-           document.getElementById(`cercel__2`).style.backgroundColor = "blue"; //cercel 2
+        if (counter == 2) {
+            document.getElementById(`line__2`).style.backgroundColor = "blue";//line 2
+            document.getElementById(`cercel__2`).style.backgroundColor = "blue"; //cercel 2
 
 
-       }
-       else{
-           document.getElementById(`line__2`).style.backgroundColor = "gray";//line 2
-           document.getElementById(`cercel__2`).style.backgroundColor = "gray"; //cercel 2
-       }
+        }
+        else {
+            document.getElementById(`line__2`).style.backgroundColor = "gray";//line 2
+            document.getElementById(`cercel__2`).style.backgroundColor = "gray"; //cercel 2
+        }
 
-        if (counter>=1 ){
+        if (counter >= 1) {
             document.getElementById(`cercel__1`).style.backgroundColor = "blue"; //cercel 1
 
         }
-        
 
-        else{
+
+        else {
             document.getElementById(`cercel__1`).style.backgroundColor = "gray"; //cercel 1
-            console.log("xhjbvc")
+            // console.log("xhjbvc")
         }
-        if(i!== counter){
+        if (i !== counter) {
 
-            
-            array_position[i].style.display="none"
-        }else{
+
+            array_position[i].style.display = "none"
+        } else {
             array_position[i].style.display = "block"
         }
     }
-   
+
 })
 
 btn_previece.addEventListener("click", (e) => {
-    if (counter >0) {
+    if (counter > 0) {
         counter -= 1
         console.log(counter)
     }
 
-    
+
     for (let i = 0; i >= 0 && i <= 2; i++) {
         if (counter == 2) {
             document.getElementById(`line__2`).style.backgroundColor = "blue";//line 2
@@ -163,73 +112,134 @@ btn_previece.addEventListener("click", (e) => {
 )
 // validation 
 
-stepper_Data__Personnel.addEventListener("input" ,(e)=>{
+// personnel information 
+stepper_Data__Personnel.addEventListener("input", () => {
+    const inputs = stepper_Data__Personnel.querySelectorAll("input, textarea, select");
+    inputs.forEach(el => {
+        if (el.name && el.value.trim() !== "") {
+            experience_objectife_pro.informations[el.name] = el.value.trim("");
+        }
+    });
+    console.log(" Informations :", experience_objectife_pro.informations);
+    localStorage.setItem("Informations", JSON.stringify(experience_objectife_pro.informations));
+});
 
-    
-console.log(counter)
-        console.log(stepper_Data__Personnel.querySelectorAll("input"))
-        let inputs_value = stepper_Data__Personnel.querySelectorAll("input , textarea,select")
-        inputs_value.forEach((el)=>{
-            if(el.value){
-// console.log(el.name)
-                experience_objectife_pro.informations[el.name] =el.value
+// parcours
+stepper_Data__competence.addEventListener("input", () => {
+    const inputs = stepper_Data__competence.querySelectorAll("input, textarea, select");
 
-            }
-        })
-    
-        console.log(personnel_information)
+    inputs.forEach(el => {
+        if (!el.name || el.value.trim() === "") return;
+        const val = el.value.trim();
 
-    })
-stepper_Data__competence.addEventListener("input", (e)=>{
-    
-        console.log(stepper_Data__competence.querySelectorAll("input"))
-        let inpute_competence = stepper_Data__competence.querySelectorAll("input , textarea, select")
-        inpute_competence.forEach((el) => {
-            // let el=e.target
-            if (el.value) {
+        if (["month_depuis", "year_depuis"].includes(el.name)) {
+            experience_objectife_pro.formation[0].depuis[el.name] = val;
+        } else if (["month_jusqua", "year_jusqua"].includes(el.name)) {
+            experience_objectife_pro.formation[0].jusqua[el.name] = val;
+        }
+        else if (["Formation_scolaire", "Etablissement", "localite"].includes(el.name)) {
+            experience_objectife_pro.parcours_educatif[el.name] = val;
+        }
 
-                experience_objectife_pro.competences[0][el.name] = el.value
+        else if (el.name === "Loisirs") {
+            experience_objectife_pro.loisirs = val.split(",").map(v => v.trim());
+        }
 
-            }
-            else{
-                return
-            }
+        else if (el.name === "section_suplementaire") {
+            experience_objectife_pro.extraSection.titre = val;
+        } else if (el.name === "text_section_suplementaire") {
+            experience_objectife_pro.extraSection.description = val;
+        }
+    });
 
-        })
-    console.log(parcours_educatif)
+    // localstorage
+    localStorage.setItem("experience_formation", JSON.stringify(experience_objectife_pro.formation));
+    localStorage.setItem("Langues", JSON.stringify(experience_objectife_pro.langues));
+    localStorage.setItem("loisirs", JSON.stringify(experience_objectife_pro.loisirs));
+    localStorage.setItem("section_suplementaire", JSON.stringify(experience_objectife_pro.extraSection));
+    localStorage.setItem("Parcours éducatif :", JSON.stringify(experience_objectife_pro.parcours_educatif))
 
-    }
-)
+    console.log("Compétences :", experience_objectife_pro.competences);
+    console.log("Langues :", experience_objectife_pro.langues);
+    console.log("Loisirs :", experience_objectife_pro.loisirs);
+    console.log("Parcours éducatif :", experience_objectife_pro.parcours_educatif);
+});
 
-stepper_Data__experience.addEventListener("input", (e) => {
-    console.log(stepper_Data__experience.querySelectorAll("input, textarea ,select"))
-    let input_experience = stepper_Data__experience.querySelectorAll("input, textarea ,select")
-    // let el = e.target
+// add languge and  competence 
+const btn = document.getElementById("addcompetenceBtn")
+btn.addEventListener("click", (e) => {
 
- 
-        // console.log(element.name)
-
-    input_experience.forEach(element => {
-            if (element.value) {
-                experience_objectife_pro.experiences[element.name] = element.value
-            } else {
-                return
-            }
+    const competencenom = document.querySelector('[name="competence"]').value;
+    const niveau = document.querySelector('[name="niveaux_competence"]').value;
+    if (competencenom != "") {
+        experience_objectife_pro.competences.push({
+            label: competencenom,
+            niveau: niveau
         });
+        document.querySelector('[name="competence"]').value = ""
+        console.log(experience_objectife_pro.competences)
+
+        localStorage.setItem("Competences", JSON.stringify(experience_objectife_pro.competences));
+    }
+
+})
+// langauge
+const addLanguageBtn = document.getElementById("addLanguageBtn")
+addLanguageBtn.addEventListener("click", (e) => {
+    const langue = document.querySelector('[name="Langue"]').value;
+    const niveau = document.querySelector('[name="niveaux_langue"]').value;
+    if (langue != "") {
+        experience_objectife_pro.langues.push({
+            label: langue,
+            niveau: niveau
+        });
+        document.querySelector('[name="Langue"]').value = ""
+
+        console.log(experience_objectife_pro.langues)
+
+        localStorage.setItem("langue", JSON.stringify(experience_objectife_pro.langues));
+    }
+
+})
+
+
+
+
+/* experience*/
+stepper_Data__experience.addEventListener("input", () => {
+    const inputs = stepper_Data__experience.querySelectorAll("input, textarea, select");
+    inputs.forEach(el => {
+        if (!el.name || el.value.trim() === "") return;
+        const val = el.value.trim();
+
+        if (el.name === "objectif_professionelle")
+            experience_objectife_pro[el.name] = val;
+
+        else if (el.name === "projects")
+            experience_objectife_pro.experiences.projets[0] = { titre: val };
+
+        else if (el.name === "projects_description")
+            experience_objectife_pro.experiences.projets[0].description = val;
+
+        else if (el.name === "certification")
+            experience_objectife_pro.experiences.certifications[0] = { titre: val };
+
+        else if (el.name === "certification_institus")
+            experience_objectife_pro.experiences.certifications[0].institution = val;
+
+        else if (el.name === "Reference_poste")
+            experience_objectife_pro.experiences.references.poste = val;
+    });
+
+    localStorage.setItem("Experiences", JSON.stringify(experience_objectife_pro.experiences));
 
     
 
-    console.log(experience_objectife_pro)
-    
-    console.log(experience_objectife_pro.experiences)
-})  
- 
-    
+});
 
-
-
-// btn_next.addEventListener("click", (e) => { console.log("hello"+counter) })
-
-const inputs = document.getElementsByClassName("input"); // NodeList
-// inputs.forEach(input => console.log(input));
-// console.log(inputs)
+// IMPLEMENTER LE TEMPLTAE
+enregistrement_btn.addEventListener("click", (e) => {
+    localStorage.setItem("cvData", JSON.stringify(experience_objectife_pro)); 
+    window.location.href = "./template.html"
+});
+// document.getElementById("extra-titre").style.backgroundColor="red"
